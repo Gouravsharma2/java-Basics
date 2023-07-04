1c48974b03f59c52edb0a00f07836d4c2fe17561
Gouravsharma2 <94118669+Gouravsharma2@users.noreply.github.com>
2023-07-04 18:23:05 +0530

    Add files via upload
    to fin binomial coefiicient

--- javabasics.java
+++ javabasics.java
@@ -8,7 +8,15 @@ public class javabasics {
     }
     return f;
   }
+  public static int bcof(int n , int r) {
+    int fact_n = factorial(n);
+    int fact_r = factorial(r);
+    int fact_nmr = factorial(n-r);
+
+    int bcof =fact_n/(fact_r * fact_nmr);
+    return bcof;
+  }
   public static void main(String args[]){
-    System.out.println (factorial(5));
+    System.out.println(bcof(5,2));
   }
 }
