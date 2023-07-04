1d89ed92588870ad4cd1679338efef481a4c28e3
Gouravsharma2 <94118669+Gouravsharma2@users.noreply.github.com>
2023-07-04 17:52:43 +0530

    Add files via upload
    factorial of any number

--- javabasics.java
+++ javabasics.java
@@ -1,25 +1,14 @@
 import java.util.*;
-public class javabasics{
-  public static int multiply () {
-    Scanner Sc = new Scanner (System.in);
-    System.out.print("A = ");
-    int a = Sc.nextInt();
-    System.out.print("B = ");
-    int b = Sc.nextInt();
-    int c = (a * b);
-    System.out.print("PRODUCT OF A AND B = ");
-    System.out.println(c);
-    return a;
+public class javabasics {
+    public static int factorial (int n) {
+      int f = 1;
+      
+    for(int i=1; i<=n; i++) {
+      f = f * i;
+    }
+    return f;
   }
-  public static void main (String args[]) {
-   // Scanner Sc = new Scanner (System.in);
-    //System.out.print("A = ");
-    //int a = Sc.nextInt();
-    //System.out.print("B = ");
-    //int b = Sc.nextInt();
-    //int c = (a * b);
-    //System.out.print("PRODUCT OF A AND B = ");
-    //System.out.println(c);
-    multiply ();
+  public static void main(String args[]){
+    System.out.println (factorial(5));
   }
 }
