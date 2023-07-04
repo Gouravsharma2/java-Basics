330689f029f59b75dba2ce30de2bd0f277320efa
Gouravsharma2 <94118669+Gouravsharma2@users.noreply.github.com>
2023-07-04 11:17:19 +0530

    Add files via upload
    multiply using perameneters

--- javabasics.java
+++ javabasics.java
@@ -1,27 +1,25 @@
 import java.util.*;
 public class javabasics{
-  public static void hellobhai(){
-    System.out.println("heelo aman");
-    System.out.println("heelo chaman");
-    System.out.println("heelo raman");
-    System.out.println("heelo naman");
-    System.out.println("heelo vidhi");
-    System.out.println("heelo ridhi");
-    System.out.println("heelo sidhi");
-    System.out.println("heelo pidi");
-    System.out.println("heelo jidi");
-    System.out.println("heelo budhi");
-    System.out.println("heelo aman love vidhi");
-  }
-  public static void sumc() {
-    Scanner Sc =new Scanner(System.in);
+  public static int multiply () {
+    Scanner Sc = new Scanner (System.in);
+    System.out.print("A = ");
     int a = Sc.nextInt();
+    System.out.print("B = ");
     int b = Sc.nextInt();
-    int sum = (a + b);
-    System.out.println(sum);
+    int c = (a * b);
+    System.out.print("PRODUCT OF A AND B = ");
+    System.out.println(c);
+    return a;
   }
   public static void main (String args[]) {
-    sumc();
-    
+   // Scanner Sc = new Scanner (System.in);
+    //System.out.print("A = ");
+    //int a = Sc.nextInt();
+    //System.out.print("B = ");
+    //int b = Sc.nextInt();
+    //int c = (a * b);
+    //System.out.print("PRODUCT OF A AND B = ");
+    //System.out.println(c);
+    multiply ();
   }
 }
